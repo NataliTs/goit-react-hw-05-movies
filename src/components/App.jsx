eb8ddcc892ae4751a608a048124e07ae8569a4c8
NataliTs <natikz20@ukr.net>
2023-05-24 11:34:33 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <GlobalStyle /> start
+    </Layout>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
+const theme = {
+  colors: {
+    text: '#212121',
+    secondaryText: 'gray',
+    white: '#fff',
+    backgroundColor: '#6a6ac3',
+    acentBgcolor: '#303f9f',
+    tableHeadColor: '#7189b2',
+    message: '#b00a0a',
+  },
+  boxShadow: '0 8px 15px rgba(0, 0, 0, .1);',
+  boxShadowHover: '0 15px 20px rgba(46, 229, 157, .4);',
+  border: '1px solid #fff',
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
